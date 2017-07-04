b47119466831dabb40882dfb87971e656e8779dc
Pushkar Kulkarni <pushkar.nk@in.ibm.com>
2017-07-04 16:51:13 +0530

    Added upload task with a handler

---
+++ HTTPUploadTask.swift
@@ -0,0 +1,33 @@
+import Foundation
+import XCTest
+
+//Upload a file, download it back and then delete it
+//Using https://www.posttestserver.com for now!
+
+class HTTPUploadTask: XCTestCase {
+    static var allTests: [(String, (HTTPUploadTask)->() throws -> Void)] {
+        return [("testUploadWithCompletionHandler", testUploadWithCompletionHandler)]
+    }
+
+    func testUploadWithCompletionHandler() {
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: nil)
+        var request = URLRequest(url: URL(string: "http://posttestserver.com/post.php")!)    
+        request.httpMethod = "POST"
+        
+        let uploadDataExpectation = expectation(description: "Upload data to http://posttestserver.com")
+        var fileData: Data!
+        do {
+            fileData =  try Data(contentsOf: URL(fileURLWithPath: "/root/pushkar/urlsession-tests/README.md"))
+        } catch {} 
+
+        let task = session.uploadTask(with: request, from: fileData) { _, response, error in
+            XCTAssertNotNil(response)
+            XCTAssertNil(error)
+            uploadDataExpectation.fulfill()
+        }
+
+        task.resume()
+        waitForExpectations(timeout: 10)
+    }
+}
+
--- main.swift
+++ main.swift
@@ -2,5 +2,6 @@ import XCTest
 
 XCTMain([
     testCase(HTTPDataTask.allTests),
-    testCase(HTTPDownloadTask.allTests)
+    testCase(HTTPDownloadTask.allTests),
+    testCase(HTTPUploadTask.allTests),
 ])
