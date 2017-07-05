30c7d3fc0e1b08370a537ad11a18571194572e77
Pushkar Kulkarni <pushkar.nk@in.ibm.com>
2017-07-05 11:53:45 +0530

    Upload task with a delegate

--- HTTPUploadTask.swift
+++ HTTPUploadTask.swift
@@ -5,14 +5,18 @@ import XCTest
 //Using https://www.posttestserver.com for now!
 
 class HTTPUploadTask: XCTestCase {
+
+    let delegate = HTTPUploadDelegate()
+
     static var allTests: [(String, (HTTPUploadTask)->() throws -> Void)] {
-        return [("testUploadWithCompletionHandler", testUploadWithCompletionHandler)]
+        return [("testUploadWithCompletionHandler", testUploadWithCompletionHandler),
+                ("testUploadWithDelegate", testUploadWithDelegate)]
     }
 
     func testUploadWithCompletionHandler() {
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: nil)
         var request = URLRequest(url: URL(string: "http://posttestserver.com/post.php")!)    
-        request.httpMethod = "POST"
+        request.httpMethod = "POST" //the service allows only POST ;(
         
         let uploadDataExpectation = expectation(description: "Upload data to http://posttestserver.com")
         var fileData: Data!
@@ -23,12 +27,54 @@ class HTTPUploadTask: XCTestCase {
         let task = session.uploadTask(with: request, from: fileData) { _, response, error in
             XCTAssertNotNil(response)
             XCTAssertNil(error)
-            //TODO: assert response code is 200
+            XCTAssertEqual((response as? HTTPURLResponse)?.statusCode, 200, "Unexpected status code")
             uploadDataExpectation.fulfill()
         }
 
         task.resume()
         waitForExpectations(timeout: 10)
     }
+
+    func testUploadWithDelegate() {
+        let session = URLSession(configuration: .default, delegate: delegate, delegateQueue: nil)
+        var request = URLRequest(url: URL(string: "http://posttestserver.com/post.php")!)
+        request.httpMethod = "POST"
+
+        delegate.responseReceivedExpectation = expectation(description: "Upload data to http://posttestserver.com")
+        delegate.uploadCompletedExpectation = expectation(description: "Upload data to http://posttestserver.com")
+
+        var fileData: Data!
+        do {
+            fileData =  try Data(contentsOf: URL(fileURLWithPath: "./README.md"))
+        } catch {}
+
+        let task = session.uploadTask(with: request, from: fileData)
+        task.resume()
+        waitForExpectations(timeout: 10)
+    }
 }
 
+class HTTPUploadDelegate: NSObject {
+    var responseReceivedExpectation: XCTestExpectation!
+    var uploadCompletedExpectation: XCTestExpectation!
+    var totalBytesSent: Int64 = 0
+}
+
+extension HTTPUploadDelegate: URLSessionTaskDelegate {
+
+    func urlSession(_ session: URLSession, task: URLSessionTask, didSendBodyData bytesSent: Int64, totalBytesSent: Int64, totalBytesExpectedToSend: Int64) {
+        self.totalBytesSent = totalBytesSent
+    }
+}
+
+extension HTTPUploadDelegate: URLSessionDataDelegate {
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive response: URLResponse, completionHandler: @escaping (URLSession.ResponseDisposition) -> Void) {
+        completionHandler(.allow)
+        responseReceivedExpectation.fulfill()
+    }
+
+    func urlSession(_ session: URLSession, dataTask: URLSessionDataTask, didReceive data: Data) {
+        XCTAssertEqual(data.count, Int(totalBytesSent))
+        uploadCompletedExpectation.fulfill()
+    }
+}
